a9c255f12cbe5c79d1c07b58031ec40a314f6f46
Challa Sumanth <challasumanth44@gmail.com>
2023-03-26 14:37:11 +0530

    commit

---
+++ src/main/java/com/sumanth/AddController.java
@@ -0,0 +1,36 @@
+package com.sumanth;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.sumanth.service.AddService;
+
+@Controller
+public class AddController {
+
+	@RequestMapping("/add")
+	//public ModelAndView add(HttpServletRequest request, HttpServletResponse response)
+	public ModelAndView add(@RequestParam("num1") int i,@RequestParam("num2") int j, HttpServletRequest request, HttpServletResponse response)
+	{
+		ModelAndView mv = new ModelAndView();
+		
+		//int i = Integer.parseInt(request.getParameter("num1"));
+		
+		//int j = Integer.parseInt(request.getParameter("num2"));
+		
+		AddService as = new AddService();
+		
+		int k = as.add(i, j);
+		
+		mv.setViewName("display");
+		mv.addObject("result",k);
+		return mv;
+		
+	}
+}
